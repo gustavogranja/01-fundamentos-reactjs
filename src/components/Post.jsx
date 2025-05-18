0267199867574a458c6fbbbc3d5d7d1c2043a10a
GUSTAVO GRANJA <gustavo.cisco235@gmail.com>
2025-05-18 13:12:57 -0300

    feat: :sparkles: Criação do Componente Avatar
    Criação do Componente Avatar
    
    Criação do Componente Avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,13 @@
 import styles from './Post.module.css';
 import { Comment } from './Comment';
+import { Avatar } from './Avatar';
 
 export function Post() {
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/gustavogranja.png"  />
+                    <Avatar  src="https://github.com/gustavogranja.png"  />
                     <div className={styles.authorInfo}>
                         <strong>Gustavo Granja</strong>
                         <span>Web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 import { PencilLine } from 'phosphor-react';
 
@@ -9,7 +10,7 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1669023414180-4dcf35d943e1?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c29mdHdhcmUlMjBkZXZlbGVwb3J8ZW58MHx8MHx8fDA%3D" 
             />
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/gustavogranja.png" />
+                <Avatar src="https://github.com/gustavogranja.png"/>
 
                 <strong>Gustavo Granja</strong>
                 <span>Web Developer</span>
